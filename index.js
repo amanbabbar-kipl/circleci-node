c2de748dfd31e6886396e387f48f1b249fb7f7c3
Amar Bhaskar <amar.b@konstantinfosolutions.com>
2022-08-03 12:35:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port =  process.env.PORT || 3000;
+
+const cars = [
+    {
+        'brand': 'Tata',
+        'modal': 'Naxon',
+        'make': '2018'
+    },
+    {
+        'brand': 'Tata',
+        'modal': 'Punch',
+        'make': '2022'
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/cars', (req, res) => {
+    res.json({cars});
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
